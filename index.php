9010ef9e3869dcd71d36317469419ac2d65ad451
jouannetaud <remijouannetaud10@gmail.com>
2021-04-02 21:11:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Jouannetaud remi" => [
+        "city" => "Lyon",
+        "weapon" => "shotgun"
+    ]
+];
+
+
+var_dump($characters["Jouannetaud remi"]);
+
+
+
